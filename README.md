8d35d0a7a4773574eadecad43d47c3675ab19f00
Riotpiaole <locartrock@gmail.com>
2019-01-25 09:16:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CSI4142 project
+
+## Conceptual Design
+
+- Build a star schema for specific business purposes
+
+- The datasets that are used are
+  1. Ottawa's collision data in [2014](http://data.ottawa.ca/dataset/collisiondata2014),[2015](http://data.ottawa.ca/dataset/collisiondata2015),[2016](http://data.ottawa.ca/dataset/collisiondata2016),[2017](http://data.ottawa.ca/dataset/collisiondata2017)
+  2. Weather data of Ottawa links [here](http://climate.weather.gc.ca/historical_data/search_historic_data_e.html)
+
---
+++ datasets/get_data.sh
@@ -0,0 +1,9 @@
+# 2014
+curl -O http://data.ottawa.ca/dataset/1e23c931-428c-4e8f-af89-cdb59162469b/resource/1df4b39f-3c6e-47e5-bed9-d3f138fff076/download/2014collisionsfinal.xls
+# 2015
+curl -O http://data.ottawa.ca/dataset/43383343-8071-4dd7-9090-410c2b78af43/resource/df46731b-31b9-4f9e-b39d-d9c49cf01a81/download/2015collisionsfinal.xls
+# 2016
+curl -O http://data.ottawa.ca/dataset/adae2dd7-43a1-4e3f-b597-44f7dd10f6a2/resource/a70c564d-1c8a-4ede-be04-bc286434b113/download/2016collisionsfinal.xls
+# 2017
+curl -O http://data.ottawa.ca/dataset/adae2dd7-43a1-4e3f-b597-44f7dd10f6a2/resource/a70c564d-1c8a-4ede-be04-bc286434b113/download/2016collisionsfinal.xls
+
